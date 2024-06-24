105026d1c5289a6c288ed7ef821a62f4bc2a815e
ylcnglygl <malozyali@gmail.com>
2024-06-24 15:20:54 +0300

    initial commit

---
+++ FlickrMobile/FlickrMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrMobile
+//
+//  Created by Yalçın Golayoğlu on 24.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
